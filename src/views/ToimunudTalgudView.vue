1db55362ff0320e20fec5e679487e75525699a36
TriinSagur <triin.sagur@hotmail.com>
2022-05-03 15:51:24 +0300

    Triin toimunud talgud

--- src/views/ToimunudTalgudView.vue
+++ src/views/ToimunudTalgudView.vue
@@ -1,15 +1,60 @@
 <template>
-<div>
-  Toimunud
-</div>
+  <div>
+    <br>
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">Kuupäev</th>
+        <th scope="col">Nimi</th>
+        <th scope="col">Asukoht</th>
+        <th scope="col"></th>
+      </tr>
+      </thead>
+
+      <tbody>
+      <tr v-for="project in projects">
+        <th scope="row">{{ project.startTime }}</th>
+        <td>{{ project.name }}</td>
+        <td>{{ project.address }}</td>
+        <td>
+          <button type="button" class="btn btn-secondary btn-sm">Lisainfo</button>
+        </td>
+      </tr>
+      </tbody>
+
+
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ToimunudTalgudView"
+  name: "ToimunudTalgudView",
+  data: function () {
+    return {
+      // project: {},
+      projects: {}
+    }
+  },
+  methods:{
+    getAllOldProjects: function (){
+      this.$http.get('/project-old')
+      .then(response=> {
+        this.projects = response.data
+        console.log(response.data)
+      })
+      .catch(error=> console.log(error))
+
+    }
+    },
+    mounted() {
+      this.getAllOldProjects()
+    }
+
+
 }
 </script>
 
-<style scoped>
+<!--<style scoped>-->
 
-</style>
+<!--</style>-->

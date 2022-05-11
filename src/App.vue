9ab4d4fa79599f686784ecfa13322595e9a8bbd0
priitrs <priitrs@gmail.com>
2022-05-11 17:36:07 +0300

    PRIIT-15 maps OK

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
   <div id="app">
+    <br>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <div class="w3-bar w3-black">
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
       </a>
-
-      <ul class="nav justify-content-left">
+      <ul class="nav justify-content-center">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">
-            <router-link to="/">Avaleht</router-link>
+            <router-link to="/"><button type="button" class="button"><span>AVALEHT</span></button></router-link>
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">
-            <router-link to="/toimunud">Toimunud Talgud</router-link>
+            <router-link to="/toimunud"><button type="button" class="button"><span>TOIMUNUD TALGUD</span></button></router-link>
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">
-            <router-link to="/tulevased">Tulevased Talgud</router-link>
+            <router-link to="/tulevased"><button type="button" class="button"><span>TULEVASED TALGUD</span></button></router-link>
           </a>
         </li>
 
@@ -60,6 +63,7 @@
     </mdb-footer>
     <!-- Footer -->
   </div>
+  </div>
 
 </template>
 <script>
@@ -72,6 +76,8 @@
 
 export default {
   name: 'App',
+
+
   data: function () {
     return {
       loginButtonText: ""
@@ -123,7 +129,44 @@ export default {
 
 
 <style>
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #000000;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 20px;
+  padding: 5px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 2px;
+}
 
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
 #app {
   font-family: var(--mdc-typography-font-family, Roboto, sans-serif);
   -webkit-font-smoothing: antialiased;
@@ -131,6 +174,7 @@ export default {
   text-align: center;
   color: #FFFFFF;
   font-size: 18px;
+
 }
 
 nav a.router-link-exact-active {
--- src/views/AvalehtView.vue
+++ src/views/AvalehtView.vue
@@ -1,18 +1,18 @@
 <template>
 
-  <div>
-
-    <br><br><br><br><br><br><br><br><br><br><br><br>
+<div>
+  <br><br><br><br><br><br><br><br><br>
 
     <p class="ml13">VIIME KOKKU VABATAHTLIKUD ÜHISE EESMÄRGI SAAVUTAMISE NIMEL.</p>
 
 
-  </div>
-
+</div>
 </template>
 
 <script>
-
+export default {
+  name: "AvalehtView"
+}
 </script>
 <style>
 
